c5b816c0ade1f6cfaab9ce3d1964e28e044b6bec
gamingyoda <dymjpn@gmail.com>
2025-11-19 19:02:43 +0900

    update kadai7

--- kadai7/kadai7.c
+++ kadai7/kadai7.c
@@ -143,6 +143,7 @@ int main(void)
     fprintf(gp, "unset surface\n");
     fprintf(gp, "set xlabel 'x'\n");
     fprintf(gp, "set ylabel 'y'\n");
+    fprintf(gp, "set object 1 rect from 30,0 to 60,40 fc rgb 'gray' fs solid 0.4 border lc rgb 'black'\n");
     fprintf(gp, "set cntrparam levels discrete 0,1,2,3,4,5,6,7,8,9,10,20,30\n");
     fprintf(gp, "splot 'psi.dat' using 1:2:3 with lines\n");
     fclose(gp);
--- kadai7/kadai7.gp
+++ kadai7/kadai7.gp
@@ -4,5 +4,6 @@ set contour base
 unset surface
 set xlabel 'x'
 set ylabel 'y'
+set object 1 rect from 30,0 to 60,40 fc rgb 'gray' fs solid 0.4 border lc rgb 'black'
 set cntrparam levels discrete 0,1,2,3,4,5,6,7,8,9,10,20,30
 splot 'psi.dat' using 1:2:3 with lines
